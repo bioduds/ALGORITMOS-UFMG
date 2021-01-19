73abbb272c53f11931e1816e4c4b2fe1929829bc
bioduds <bioduds@gmail.com>
2021-01-19 18:59:50 +0000

    start fetching input

--- Algoritmos I/TP1/covid.cpp
+++ Algoritmos I/TP1/covid.cpp
@@ -77,14 +77,21 @@ void Graph::DFS( int v ) {
 // Driver code
 int main() {
 
+    // begin read of input
+    int c, p, x;
+
+    cin >> c >> p >> x;
+
+    cout << "Peguei: " << c << " - " << p << " - " << x << endl;
+    
 	// Create a graph given in the above diagram
-	Graph g(4);
-	g.addEdge(0, 1);
-	g.addEdge(0, 2);
-	g.addEdge(1, 2);
-	g.addEdge(2, 0);
-	g.addEdge(2, 3);
-	g.addEdge(3, 3);
+	Graph g( 4 );
+	g.addEdge( 0, 1 );
+	g.addEdge( 0, 2 );
+	g.addEdge( 1, 2 );
+	g.addEdge( 2, 0 );
+	g.addEdge( 2, 3 );
+	g.addEdge( 3, 3 );
 	cout << "Following is Depth First Traversal (starting from vertex 2) \n";
 	g.DFS( 2 );
     cout << endl;
