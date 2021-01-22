641ad72583fff12cf18494cb31ef44f346c993f2
bioduds <bioduds@gmail.com>
2021-01-22 03:01:08 +0000

    Commenting Code

--- Algoritmos I/TP1/covid.cpp
+++ Algoritmos I/TP1/covid.cpp
@@ -3,127 +3,78 @@
 ** Contato: eduardocapanema@ufmg.br
 ** Matrícula: 2020041515
 ** Disciplina: Algoritmos I
+** Prof: Jussara Marques
 */
 
-// C++ program to print DFS traversal from
-// a given vertex in a given graph
 #include<iostream>
 #include <sstream>
 #include<list>
 #include <vector>
 #include <algorithm>
-
 using namespace std;
 
-
-// Graph class represents a directed graph
-// using adjacency list representation
+// Classe que representa o grafo direcionado usando lista de adjacências
 class Graph {
-
-    int V; // No. of vertices
-  	// Pointer to an array containing
-	// adjacency lists
-	list<int> * adj;
-	// A recursive function used by DFS
-	void DFSUtil( int v, bool visited[], int f );
-
+    int V; // Número de Vértices
+	list<int> * adj; // Ponteiro para array com a lista de adjacências
+	void DFSUtil( int v, bool visited[], int f ); // Função recursiva do DFS
   public:
-	Graph( int V ); // Constructor
-	// function to add an edge to graph
-	void addEdge( int v, int w );
-	// DFS traversal of the vertices
-	// reachable from v
-	void DFS( int v );
-    vector<int> path;
-    int fuel;
-    int hasCycle = 0;
-
+	Graph( int V ); // Nosso construtor
+	void addEdge( int v, int w ); // Função que adiciona arestas ao grafo
+	void DFS( int v ); // Travessia nos vértices a partir de v
+    vector<int> path; // vetor contendo os vértices nas rotas
+    int fuel; // Qte de postos alcançáveis a partir de um Centro de Distribuição
+    int hasCycle = 0; // 1 quando revisitamos um vértice
 };
 
 Graph::Graph( int V ) {
-	
-    this->V = V; // assign do construtor
-	adj = new list<int>[V]; // iniciamos uma lista de adjacencia
-
+    this->V = V;
+	adj = new list<int>[V]; // iniciamos a lista de adjacencia
 }
 
 void Graph::addEdge( int v, int w ) {
-
-	adj[v].push_back( w ); // Add w to v’s list.
-
+	adj[v].push_back( w ); // Adicionamos w à lista de v’s.
 }
 
 void Graph::DFSUtil( int v, bool visited[], int f=0 ) {
-
-    //cout << "DFSUtil fuel: " << f << endl;
-	// Mark the current node as visited and
-	// print it
-	visited[v] = true;
-	//cout << v << " ";
-    path.push_back( v );
-	// Recur for all the vertices adjacent
-	// to this vertex
-	list<int>::iterator i;
+	visited[v] = true; // Marcamos o vértice atual como visitado
+    path.push_back( v ); // incluímos o vértice visitado ao path
+	list<int>::iterator i; // Recorrência nos vértices adjacentes
 	for( i = adj[v].begin(); i != adj[v].end(); ++i ) {
-        //cout << "Checing iter: " << *i << endl;
   		if( !visited[*i] ) {
-            //cout << "Recursivando..." << endl;
             int nFuel = f-1;
-            if( nFuel >= 0 ){ DFSUtil( *i, visited, nFuel ); }
-        }
+            if( nFuel >= 0 ){ DFSUtil( *i, visited, nFuel ); } // chamada recursiva
+        } else { hasCycle = 1; } // por teoria, uma revisita configura um ciclo
     }
-
 }
 
-// DFS traversal of the vertices reachable from v.
-// It uses recursive DFSUtil()
-void Graph::DFS( int v ) {
-
-    //cout << "Doing DFS com fuel: " << fuel << endl;
-
-	// Mark all the vertices as not visited
-	bool *visited = new bool[V];
-	for( int i = 0; i < V; i++ ) { 
-        visited[i] = false; 
-    }
-	// Call the recursive helper function
-	// to print DFS traversal
-	DFSUtil( v, visited, fuel );
-
+void Graph::DFS( int v ) { // Método Principal que realiza a travessia pelos vértices
+	bool *visited = new bool[V]; // Marca todos os vértices como não visitados
+	for( int i = 0; i < V; i++ ) { visited[i] = false; }
+	DFSUtil( v, visited, fuel ); // Inicia a chamada do método recursivo
 }
 
-// Driver code
+/************* FUNÇÃO MAIN *************/
 int main() {
-
     int c, p, x;
     cin >> c >> p >> x;
-    Graph g( p + c );
-    vector<string> pLines; 
+    Graph g( p + 1 ); // Incluímos 1 para os CDs - todos na posição 0
     for( int i=0; i <= ( p + c ); i++ ) {
-        string line;
-        getline( cin, line );
-        istringstream is( line );
+        string line; getline( cin, line ); istringstream is( line );
         int n;
         while( is >> n ) {
-            if( i <= c ) { g.addEdge( 0, n ); } 
-            else {
-                if( n != 0 ) { g.addEdge( ( i - 2 ), n ); }
-            }
+            if( i <= c ) { g.addEdge( 0, n ); } // adicionamos vértices para os CDs
+            else { if( n != 0 ) { g.addEdge( ( i - 2 ), n ); } } // adicionamos os demais vértices
         }
     }
-
-    // calc fuel
-    g.fuel = 30/x;
-    //cout << "x: " << x << " 30/x (fuel): " << g.fuel << endl;
-    g.DFS( 0 );
-    sort( g.path.begin(), g.path.end() );
-    cout << g.path.size() - 1 << endl;
-    for( int j=1; j < g.path.size(); j++ ) {
-        cout << g.path[j] << " ";
-    }
+    g.fuel = 30/x; // Calculamos o "fuel" - qte de postos que cada saída permite visitar
+    g.DFS( 0 ); // Chamamos o método principal de nossa implementação
+    sort( g.path.begin(), g.path.end() ); // realizamos uma ordenação conforme solicitado
+    cout << g.path.size() - 1 << endl; // imprimimos o número de visitas
+    for( int j=1; j < g.path.size(); j++ ) { cout << g.path[j] << " "; } // lançamos os pontos
     cout << endl;
-    cout << g.hasCycle << endl;
+    cout << g.hasCycle << endl; // dizemos se há ciclos
 
-	return 0;
+	return 0; // finalizamos nossa implementação
     
 }
