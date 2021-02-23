9a4f9ccc1681fa9a963f1408312f0470b0204088
bioduds <bioduds@gmail.com>
2021-02-23 04:55:10 +0000

    corrigindo identação

--- Algoritmos I/TP2/belleville.cpp
+++ Algoritmos I/TP2/belleville.cpp
@@ -10,17 +10,14 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-// STL implementation of Prim's algorithm for MST
 # define INF 0x3f3f3f3f
-// iPair ==>  Integer Pair
 typedef pair<int, int> iPair;
  
 // This class represents a directed graph using adjacency list representation
 class Graph {
   private:
     int V; // No. of vertices
-    // In a weighted graph, we need to store vertex
-    // and weight pair for every edge
+    // In a weighted graph, we need to store vertex and weight pair for every edge
     list<pair<int, int>> * adj;
   public:
     Graph( int V );  // Constructor
@@ -44,44 +41,34 @@ void Graph::addEdge( int u, int v, int w ) {
  
 // Prints shortest paths from src to all other vertices
 void Graph::primMST() {
-    // Create a priority queue to store vertices that
-    // are being preinMST. This is weird syntax in C++.
-    // Refer below link for details of this syntax
-    // http://geeksquiz.com/implement-min-heap-using-stl/
+    // Create a priority queue to store vertices that are being preinMST. This is weird syntax in C++.
+    // Refer below link for details of this syntax http://geeksquiz.com/implement-min-heap-using-stl/
     priority_queue<iPair, vector<iPair>, greater<iPair>> pq;
     int src = 0; // Taking vertex 0 as source
-    // Create a vector for keys and initialize all
-    // keys as infinite (INF)
+    // Create a vector for keys and initialize all keys as infinite (INF)
     vector<int> key( V, INF );
     // To store parent array which in turn store MST
     vector<int> parent( V, -1 );
     // To keep track of vertices included in MST
     vector<bool> inMST( V, false );
-    // Insert source itself in priority queue and initialize
-    // its key as 0.
+    // Insert source itself in priority queue and initialize its key as 0.
     pq.push( make_pair( 0, src ) );
     key[src] = 0;
     /* Looping till priority queue becomes empty */
     int sum = 0;
     while( !pq.empty() ) {
-        // The first vertex in pair is the minimum key
-        // vertex, extract it from priority queue.
-        // vertex label is stored in second of pair (it
-        // has to be done this way to keep the vertices
-        // sorted key (key must be first item
-        // in pair)
+        // The first vertex in pair is the minimum key vertex, extract it from priority queue. vertex label is stored 
+        // in second of pair (it has to be done this way to keep the vertices sorted key (key must be first item in pair)
         int u = pq.top().second;
         pq.pop(); 
         inMST[u] = true;  // Include vertex in MST
         // 'i' is used to get all adjacent vertices of a vertex
         list<pair<int, int>>::iterator i;
         for( i = adj[u].begin(); i != adj[u].end(); ++i ) {
-            // Get vertex label and weight of current adjacent
-            // of u.
+            // Get vertex label and weight of current adjacent of u.
             int v = ( *i ).first;
             int weight = ( *i ).second;
-            //  If v is not in MST and weight of (u,v) is smaller
-            // than current key of v
+            //  If v is not in MST and weight of (u,v) is smaller than current key of v
             if( inMST[v] == false && key[v] > weight ) {
                 // Updating key of v
                 key[v] = weight;
